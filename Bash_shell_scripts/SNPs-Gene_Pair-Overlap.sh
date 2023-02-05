6b37b43fc8d6d1de4505aa67219d1d2e3fb30e2b
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2023-02-05 17:02:41 -0600

    Add files via upload

---
+++ Bash_shell_scripts/SNPs-Gene_Pair-Overlap.sh
@@ -0,0 +1,102 @@
+#    venn                Venn diagram of intersection of genomic regions or list sets (upto 6-way).
+#    upset               UpSet diagram of intersection of genomic regions or list sets.
+#    pairwise            Pairwise intersection and heatmap of N genomic region sets in <BED/GTF/GFF> format.
+
+
+A0=1_BA9.txt.SNP-Phe-Pair.txt
+A1=2_BA24.txt.SNP-Phe-Pair.txt
+A2=3_C.txt.SNP-Phe-Pair.txt
+A3=4_H.txt.SNP-Phe-Pair.txt
+A4=5_T.txt.SNP-Phe-Pair.txt
+
+outdir="SNPs-Gene_Pair-Overlap"
+mkdir  $outdir 
+
+
+## venn
+##############################################################################
+outpath1=$outdir/venn
+mkdir  $outpath1
+intervene  venn  --input  \
+$A0  \
+$A1  \
+$A2  \
+$A3  \
+$A4  \
+--type list    --save-overlaps   --output $outpath1  \
+--dpi 1200  --figtype pdf   --figsize 12 12  --fontsize 10
+##############################################################################
+
+
+## upset
+##############################################################################
+outpath2=$outdir/upset
+mkdir  $outpath2
+intervene  upset  --input  \
+$A0  \
+$A1  \
+$A2  \
+$A3  \
+$A4  \
+--type   list   --save-overlaps   --output $outpath2  --ninter 100       \
+--dpi 1200  --figtype pdf   --figsize 12 6   
+##############################################################################
+
+
+## pairwise
+##############################################################################
+outpath1="$outdir/pairwise.fraction"
+mkdir -p $outpath1
+
+intervene  pairwise  \
+--input  \
+$A0  \
+$A1  \
+$A2  \
+$A3  \
+$A4  \
+--type list    --compute  frac  --htype color   --output $outpath1  \
+--dpi 1200  --figtype svg   --figsize 10 10  --fontsize 10
+
+
+
+outpath2="$outdir/pairwise.count"
+mkdir -p $outpath2
+
+intervene  pairwise  \
+--input  \
+$A0  \
+$A1  \
+$A2  \
+$A3  \
+$A4  \
+--type list    --compute  count  --htype color  --output $outpath2  \
+--dpi 1200  --figtype svg   --figsize 10 10   --fontsize 10
+##############################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
---
+++ Bash_shell_scripts/uniq.sh
@@ -0,0 +1,7 @@
+sort   1_BA9.txt.SNP-Only.txt | uniq | wc -l
+sort   2_BA24.txt.SNP-Only.txt | uniq | wc -l
+sort   3_C.txt.SNP-Only.txt | uniq | wc -l
+sort   4_H.txt.SNP-Only.txt | uniq | wc -l
+sort   5_T.txt.SNP-Only.txt | uniq | wc -l
+
+
