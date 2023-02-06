9e4dc6b0a6bc2ab362151800d799c224ed746d5f
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2023-02-05 21:08:23 -0600

    Add files via upload

---
+++ R_scripts/2_Anno_v20210106.sh
@@ -0,0 +1,14 @@
+
+
+ Rscript 2_Anno_v20210106.R    \
+        --inputDir=BED/sorted      \
+        --outDir=2_Annotation    \
+        --refGenome=hg38  \
+        --stranded=TRUE    \
+        --fast=TRUE        \
+        --upstream=1000      \
+        --downstream=100  
+ 
+
+
+
