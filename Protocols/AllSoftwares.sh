24da571a816ce0ece03d20e57448bc23fc4f24c1
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2023-02-13 13:28:46 -0600

    Create AllSoftwares.sh

---
+++ Protocols/AllSoftwares.sh
@@ -0,0 +1,64 @@
+#################################################################################################   
+## 1_Pre-align  33+27
+AdapterRemoval   --version
+barcode_split_trim.pl  --version
+biobloommaker    --version
+bustools | grep bustools
+csvtk  version
+defastq  2>&1  | grep version
+falco  --version
+FaQCs  --version
+fasten_regex  --version
+fastp   --version
+fastq-multx  2>&1 | grep Version
+fastq-scan   -v
+fastq_screen    --version
+fastq-uniq      --version   ## fastq-tools
+fastq_pre_barcodes   ## fastq_utils
+fastqc  --version
+FASTQuick    2>&1  |  grep Version
+fastv  --version
+fastx_clipper  -h   | grep "0.0" ## fastx_toolkit
+kmc   | grep "ver. "
+kmer-db | grep "version"
+MinIONQC.R  -h
+mutscan   --help
+prinseq-lite.pl  --version
+prinseq++   --version
+qc3.pl  -h  2>&1  | grep Version
+seqkit   version
+seqtk
+seqyclean  -h  | grep "Version"
+fastq-dump    --version     ## sratoolkit
+trim_galore   --version
+trimmomatic.jar  -version  ## java -jar /media/yp/1one/MyProgramFiles/1_Pre-align/Trimmomatic/trimmomatic.jar   -version
+unikmer  version
+
+library( "ngsReports" )
+library( "seqTools" )
+library( "Rqc" )
+library( "systemPipeR" )
+library( "qrqc" )
+library( "ShortRead" )
+kb -h
+cutadapt --version
+multiqc  --version
+fastools -v
+fastqp -h
+fqtools -version
+umi_tools  --version
+fastaq   version   ##pyfastaq
+pycoQC  --version
+cdna_classifier.py  -h  ## pychopper
+python -m fast2q -h ##
+pyfastx  --version
+python -m interop --test  ##import fastq as fq  ## python3
+fastq-stat --help
+checkqc --version
+pheniqs  --version
+fsplit  -h
+barcode_splitter   --version
+pod5-inspect  -h
+bonito basecaller
+remora  infer  -h
+
