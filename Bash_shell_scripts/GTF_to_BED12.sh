63b43e504a1c19ef966ff1dc632ca097f7413eec
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2023-02-27 19:29:16 -0600

    Add files via upload

---
+++ Bash_shell_scripts/GTF_to_BED12.sh
@@ -0,0 +1,4 @@
+
+
+bedparse  gtf2bed   Human_GRCh38.p13/gencode.v43.primary_assembly.annotation.gtf > human.gencode.v43.primary_assembly.annotation.bed
+bedparse  gtf2bed   Mouse_GRCm39/gencode.vM32.primary_assembly.annotation.gtf   > mm39.gencode.vM32.primary_assembly.annotation.bed
---
+++ Bash_shell_scripts/chrom.size.sh
@@ -0,0 +1,5 @@
+##pip install pyfaidx
+
+faidx   GRCh38.primary_assembly.genome.fa   -i chromsizes  >   hg38.sizes.genome
+
+
