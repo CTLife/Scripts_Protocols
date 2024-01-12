c50b1d5e4b58f558165c274ec1421a02b427e534
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2024-01-12 15:51:51 -0600

    Add files via upload

---
+++ R_scripts/RADAR_diff.sh
@@ -0,0 +1,10 @@
+nohup  Rscript RADAR_diff.R   -s /media/RAID69TB/yongpeng/3_Brain_QTLs_FinalVersion_noXY/Others/Groups_Types_93.txt  -i ../../../1A_m6Alevel_matrix/m6A-Peaks.All-93.no_zscore_quantile.noXY.bed           -o Differential       -t region  -a T  -b others     >  RADAR_diff.runLog.txt 2>&1    &     
+
+
+
+
+
+
+
+
+
