28483ea0e71a27bcf578cd5fd478265c621ad82a
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2024-01-28 13:21:49 -0600

    Add files via upload

---
+++ Perl_scripts/For_APAIQ/BAM2BG_RPM.sh
@@ -0,0 +1,4 @@
+nohup time  perl  BAM2BG_RPM.pl    -in ../1A_BAM/Input_minus   -out BG_RPM/Input_minus    > BAM2BG_RPM.Input_minus.runLog  2>&1   &
+nohup time  perl  BAM2BG_RPM.pl    -in ../1A_BAM/Input_plus    -out BG_RPM/Input_plus     > BAM2BG_RPM.Input_plus.runLog  2>&1   &
+
+
