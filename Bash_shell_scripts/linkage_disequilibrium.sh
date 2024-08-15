707ee5b207ad1a9368c97f2e150da5b889a3f51f
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2024-08-15 07:30:23 -0500

    Add files via upload

---
+++ Bash_shell_scripts/linkage_disequilibrium.sh
@@ -0,0 +1,17 @@
+prefix1=BA24_samples
+vcftools  --vcf 8_selectSNPs/$prefix1.recode.vcf   --hap-r2     --ld-window-bp 10000   --out 10_LD/$prefix1.ld_10kb  > 10_LD/$prefix1.runLog.txt 2>&1     
+
+prefix1=BA9_samples
+vcftools  --vcf 8_selectSNPs/$prefix1.recode.vcf   --hap-r2     --ld-window-bp 10000   --out 10_LD/$prefix1.ld_10kb  > 10_LD/$prefix1.runLog.txt 2>&1     
+
+prefix1=C_samples
+vcftools  --vcf 8_selectSNPs/$prefix1.recode.vcf   --hap-r2     --ld-window-bp 10000   --out 10_LD/$prefix1.ld_10kb  > 10_LD/$prefix1.runLog.txt 2>&1     
+
+prefix1=H_samples
+vcftools  --vcf 8_selectSNPs/$prefix1.recode.vcf   --hap-r2     --ld-window-bp 10000   --out 10_LD/$prefix1.ld_10kb  > 10_LD/$prefix1.runLog.txt 2>&1     
+
+prefix1=T_samples
+vcftools  --vcf 8_selectSNPs/$prefix1.recode.vcf   --hap-r2     --ld-window-bp 10000   --out 10_LD/$prefix1.ld_10kb  > 10_LD/$prefix1.runLog.txt 2>&1     
+
+
+
